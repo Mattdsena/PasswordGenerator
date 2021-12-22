cb9a3b73de99a1db485e759c7130b5c1ca769170
Matt DSena <matthewdsena@gmail.com>
2021-12-22 15:45:34 +1030

    got the first prompt

--- Develop/script.js
+++ Develop/script.js
@@ -9,16 +9,12 @@ function writePassword() {
   passwordText.value = password;
 
   function generatePassword() {
-    var length = 8,
-        charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
-        retVal = "";
-    for (var i = 0, n = charset.length; i < length; ++i) {
-        retVal += charset.charAt(Math.floor(Math.random() * n));
+     let userChoice = window.prompt("How many characters would you like your password to contain?");
     }
-    return retVal;
-}
+    
+  }
 
-}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
